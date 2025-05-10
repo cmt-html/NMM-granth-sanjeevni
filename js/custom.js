058b7a120f6aff40bbb48548d5e60aab1613a6ed
cloudmojo <cloudmojotech@gmail.com>
2025-05-10 19:41:49 +0530

    Final Axcept Mobile nemu

--- js/custom.js
+++ js/custom.js
@@ -78,27 +78,110 @@ document.querySelectorAll(".toggle-password").forEach(function (icon) {
   });
 });
 
+const overlay_div = `
+  <div id="overlay">
+        <div class="container">
+            <div class="static-modal">
 
+                <div class="modal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Welcome!<br> Please enter your secret key to continue.</h5>
+                            </div>
+                            <div class="modal-body pt-0">
+                                <form id="secret-overlay-form">
+                                    <div class="form-group">
+                                        <!-- <label for="login-email">Username*</label> -->
+                                        <input type="password" id="overlay_password" class="form-control" placeholder="Enter here" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="bg-fill prev-btn">Continue</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
 
+const tempDiv = document.createElement("div");
+tempDiv.innerHTML = overlay_div;
+document.body.appendChild(tempDiv.firstElementChild);
 
+//
 
 const overlay = document.getElementById("overlay");
 
 // Define your correct password (consider more secure alternatives for production)
 const correctPassword = "Admin@123"; // Replace with your actual password
 
-// Check authentication
-if (!sessionStorage.getItem("authenticated")) {
-  overlay.style.display = "block"; // Show overlay before prompt
-  const userInput = prompt("Enter the website password:");
+document
+  .getElementById("secret-overlay-form")
+  .addEventListener("submit", (event) => {
+    // Check authentication
+    const userInput = document.querySelector("#overlay_password").value;
 
-  if (userInput === correctPassword) {
-    sessionStorage.setItem("authenticated", "true");
-    overlay.style.display = "none";
-  } else {
-    alert("Incorrect password. You will be redirected.");
-    window.location.href = "/";
-  }
+    if (!sessionStorage.getItem("authenticated")) {
+      overlay.style.display = "flex"; // Show overlay before prompt
+      // const userInput = prompt("Enter the website password:");
+
+      if (userInput === correctPassword) {
+        sessionStorage.setItem("authenticated", "true");
+        overlay.style.display = "none";
+      } else {
+        alert("Incorrect password.");
+        window.location.href = "/";
+      }
+    } else {
+      overlay.style.display = "none";
+    }
+  });
+
+if (!sessionStorage.getItem("authenticated")) {
+  overlay.style.display = "flex"; // Show overlay before prompt
+  // const userInput = prompt("Enter the website password:");
 } else {
   overlay.style.display = "none";
 }
+
+// const userInput = document.querySelector("#overlay_password").value;
+//   if (userInput === correctPassword) {
+//     sessionStorage.setItem("authenticated", "true");
+//     overlay.style.display = "none";
+//   } else {
+//     alert("Incorrect password. You will be redirected.");
+//     overlay.style.display = "flex";
+//     window.location.href = "/";
+//   }
+
+// mobile menu js
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth <= 767) {
+    (function () {
+      const login_options = document.querySelector(".login-options ul");
+      const nav = document.querySelector("nav");
+      const mobileNav = document.querySelector(".mobile-menu .menu-wrraper");
+
+      if (mobileNav && nav && login_options) {
+        mobileNav.append(nav, login_options);
+      }
+
+      const hamburger = document.querySelector(".hamburger");
+      const close = document.querySelector(".mobile-menu .close");
+      const mobile_menu = document.querySelector(".mobile-menu");
+
+      $(hamburger).on("click", function () {
+        $(mobile_menu).toggleClass("active");
+      });
+
+      $(close).on("click", function () {
+        $(mobile_menu).removeClass("active");
+      });
+    })();
+  }
+});
